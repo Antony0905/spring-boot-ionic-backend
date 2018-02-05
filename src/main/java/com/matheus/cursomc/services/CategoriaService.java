1552a40af2d07b1bb64016792a833510d4a90c8b
Matheus Antony <matheus.antony@hotmail.com>
2018-02-04 23:47:42 -0200

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/matheus/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.matheus.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheus.cursomc.domain.Categoria;
+import com.matheus.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+
+		Categoria obj = repo.findOne(id);
+		
+		return obj;
+	}
+
+}
