0f83b158d35aae3c1016fa9aefb3482fc84e6295
Matheus Antony <matheus.antony@hotmail.com>
2018-02-14 02:54:49 -0200

    Inserindo Pedido

--- src/main/java/com/matheus/cursomc/domain/ItemPedido.java
+++ src/main/java/com/matheus/cursomc/domain/ItemPedido.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPk id = new ItemPedidoPk();
@@ -35,14 +35,22 @@ public class ItemPedido implements Serializable {
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
 
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 	public ItemPedidoPk getId() {
--- src/main/java/com/matheus/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/matheus/cursomc/domain/PagamentoComCartao.java
@@ -2,12 +2,14 @@ package com.matheus.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.matheus.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer numeroDeParcelas;
 
 	public PagamentoComCartao() {
