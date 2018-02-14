b2cda31f5080248a20d6703473cf6b1db3aff094
Matheus Antony <matheus.antony@hotmail.com>
2018-02-13 23:47:54 -0200

    Validacao customizada email não repetido na validação do cliente

--- src/main/java/com/matheus/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/matheus/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.matheus.cursomc.domain.Cliente;
 import com.matheus.cursomc.domain.enums.TipoCliente;
 import com.matheus.cursomc.dto.ClienteNewDTO;
+import com.matheus.cursomc.repositories.ClienteRepository;
 import com.matheus.cursomc.resources.exceptions.FieldMessage;
 import com.matheus.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-	
+
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -24,11 +31,16 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
 		}
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
-		
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
